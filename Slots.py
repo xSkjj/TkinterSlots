6f2006b7409d9c3058c53ed086e67eb3766e137d
xSkjj <xSkj@mail.de>
2020-10-01 08:12:30 +0200

    Update Slots.py

--- Slots.py
+++ Slots.py
@@ -1,17 +1,17 @@
 from tkinter import *
 
-# window settings ---
-icon = r".\assets\icon.ico"
-title = "Slots"
-minWidth = 400
-minHeight = 400
-maxWidth = 400
-maxHeight = 500
-windowWidth = "400"
-windowHeight = "400"
-offsetx = "200"
-offsety = "200"
-bgColor = "#202020"
+# window settings
+icon = r".\assets\icon.ico" # icon in the top left corner
+title = "Slots"             # Title at the top of the window
+minWidth = 400              # minimum width you can resize the window to
+minHeight = 400             # minimum height you can resize the window to
+maxWidth = 400              # maximum width you can resize the window to
+maxHeight = 500             # maximum height you can resize the window to
+windowWidth = "400"         # the width of the window when starting
+windowHeight = "400"        # the height of the window when starting
+offsetx = "200"             # distance between the window's and screen's left edge
+offsety = "200"             # distance between the window's and screen's top edge
+bgColor = "#202020"         # background color of the window
 
 # apply window settings
 root = Tk()
@@ -23,16 +23,22 @@ root.geometry((windowWidth + "x" + windowHeight + "+" + offsetx + "+" + offsety)
 root["background"] = bgColor
 
 
-# define functions --
-def trySpin(amt):
+
+# define functions
+def trySpin():
+    amt = amtInput.get()
     try:
         int(amt)
-        amtInput["bg"] = "#404040"
+        amtInput.configure(bg = "#404040")
+        print(amt)
     except:
         if amt == "":
-            amtInput["bg"] = "#404040"
+            amtInput.configure(bg = "#404040")
+            print(amt)
         else:
-            amtInput["bg"] = "#a00000"
+            amtInput.configure(bg = "#a00000")
+            print(amt)
+
 
 
 # make elements and set their properties
@@ -58,12 +64,13 @@ amtInputLabel = Label(userInputs,
                       bg   = bgColor)
 
 amtInput = Entry(userInputs,
-                 bg     = "#404040",
-                 fg     = "white",
-                 relief = FLAT)
+                 bg               = "#404040",
+                 insertbackground = "white",
+                 fg               = "white",
+                 relief           = FLAT)
 
 spinBtn = Button(userInputs,
-                 command = trySpin(amtInput.get()),
+                 command = trySpin,
                  text    = "spin",
                  font    = "Arial 10 bold",
                  fg      = "#050",
@@ -77,8 +84,10 @@ userInputs.pack()
 amtInputLabel.grid(column=0, row=0, padx=5)
 amtInput.grid     (column=1, row=0, padx=5)
 spinBtn.grid      (column=2, row=0, padx=5)
+amtInput.insert(0, "0")
 
 # bind event listeners to elements
+# None
 
 
 root.mainloop()
